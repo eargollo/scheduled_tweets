4060f08c52d3983c177571a6492bceb957b87fea
Eduardo Argollo <eargollo@gmail.com>
2023-04-22 13:31:13 -0700

    lesson 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@
 # password_confirmation:string
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'invalid email address provided' }
 end
