cc8a6412cf314c7a634223acb49f5988dfbcd135
Eduardo Argollo <eargollo@gmail.com>
2023-05-06 12:40:25 -0700

    Lesson 17

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "main#index"
 
   get "about", to: "about#index"
+
   get  "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
-  root "main#index"
+
+  get  "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
   # Defines the root path route ("/")
   # root "articles#index"
 end
